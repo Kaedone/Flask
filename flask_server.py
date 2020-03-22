4673098ab4a65ec60d5d961bfacbe86556d80e29
Тимофей Стабуль <timstab1@gmail.com>
2020-03-22 14:31:15 +0300

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -1,8 +1,12 @@
 from flask import Flask, escape, url_for, render_template
+from waitress import serve
 from start import get_table
+
 app = Flask(__name__)
 @app.route('/<param>')
 def hello(param):
 	return str(get_table(param))
 if __name__ == "__main__":
+
+    serve(app, port=8080)
     app.run(ssl_context='adhoc')
