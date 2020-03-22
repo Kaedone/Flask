3e2de0e4f6d4f9b24e20aa4cba20a4c35fe57ef3
root <timstab1@gmail.com>
2020-03-22 14:26:06 +0300

    From Linux to local

--- flask_server.py
+++ flask_server.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask, escape, url_for, render_template
+from waitress import serve
 from start import get_table
+
 app = Flask(__name__)
-@app.route('/<param>')
+@app.route('/get<param>')
 def hello(param):
 	return str(get_table(param))
 if __name__ == "__main__":
+    serve(app, host = "0.0.0.0", port = "8080")
     app.run(ssl_context='adhoc')
