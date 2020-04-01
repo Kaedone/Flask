521d5ddc8b6ca058e1d82def772288655102cc31
Kaedone <56879035+Kaedone@users.noreply.github.com>
2020-04-01 21:23:06 +0300

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask, escape, url_for, render_template, request
-from waitress import serve
-from start import get_table
+from flask import Flask
+
+from start import get_teachers, get_schedule
 
 app = Flask(__name__)
 
 
-@app.route('/get')
-def hello():
-    teachers = request.args.get('page', default=0, type=int)
-    return get_table(teachers)
+@app.route('/schedule/<schedule_id>')
+def hello(schedule_id):
+    return get_schedule(int(schedule_id))
+
+
+@app.route('/all_teachers')
+def teacher_handler():
+    return get_teachers()
 
 
 if __name__ == "__main__":
