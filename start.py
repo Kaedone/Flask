8204160ad8e38855ea5255819dfd3ff79c09c68a
Kaedone <56879035+Kaedone@users.noreply.github.com>
2020-04-17 23:28:58 +0300

    back to the standart

--- start.py
+++ start.py
@@ -17,11 +17,11 @@ SAMPLE_SPREADSHEET_ID = '1AwBuzkjz76_TCnVCKJMq8UL-ObRBC6m8BI3-NXeKiNI'
 
 creds = None
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'credentials.json')
-filename1 = os.path.join(dirname, 'token.pickle')
+route_to_credentials = os.path.join(dirname, 'credentials.json')
+route_to_token = os.path.join(dirname, 'token.pickle')
 # Открываем token.pickle в котором лежит наши авторизационные данные
-if os.path.exists(filename1):
-    with open(filename1, 'rb') as token:
+if os.path.exists(route_to_token):
+    with open(route_to_token, 'rb') as token:
         creds = pickle.load(token)
 # Если его нет, то открываем браузер и просим войти, что бы создать новый token.pickle
 if not creds or not creds.valid:
@@ -29,13 +29,13 @@ if not creds or not creds.valid:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            filename, SCOPES)
+            route_to_credentials, SCOPES)
         creds = flow.run_local_server(port=0)
     # Сохраняем учетные данные для следующего запуска
-    with open(filename1, 'wb') as token:
+    with open(route_to_token, 'wb') as token:
         pickle.dump(creds, token)
 service = build('sheets', 'v4', credentials=creds)
-sheet = service.spreadsheets()
+sheets = service.spreadsheets()
 
 
 # Получаем список всех учителей
@@ -44,14 +44,14 @@ def get_teachers():
     range_teachers_id_name = 'Test!B4:D49'
 
     # Создаём массив с результатами
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=range_teachers_id_name).execute()
+    result = sheets.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                 range=range_teachers_id_name).execute()
     values = result.get('values', [])
 
     # Делаем крассивый вывод
-    sort = json.dumps(values, sort_keys=True, indent=4)
+    lists = json.dumps(values, sort_keys=True, indent=4)
 
-    return sort
+    return lists
 
 
 # Получаем рассписание для отдельного учителя
@@ -64,8 +64,8 @@ def get_schedule(teacher_id):
     teacher_id = str(teacher_id)
     range_teachers_id_name = 'Test!B{0}:CQ{0}'.format(teacher_id)
 
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=range_teachers_id_name).execute()
+    result = sheets.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                 range=range_teachers_id_name).execute()
     values = result.get('values', [])
     # Генерируем из этого словарь для удобности вывода
     res = {
@@ -90,8 +90,8 @@ def timing():
     time_of_lessons = 'Test!U2:AN2'
 
     # Создаём массив с результатами
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=time_of_lessons).execute()
+    result = sheets.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                 range=time_of_lessons).execute()
     values = result.get('values', [])
 
     sort = {
@@ -101,29 +101,6 @@ def timing():
     # Генерим крассивый .json
     sort = json.dumps(values, sort_keys=True, indent=4)
 
-    return sort
-
-
-def schedule():
-    block = []
-    days = []
-    alf = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-           'V',
-           'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO',
-           'AP', 'AQ', 'AR', 'AS', 'AT']
-
-    time_of_lessons = 'school!C7:AT41'
-    # Создаём массив с результатами
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=time_of_lessons).execute()
-    values = result.get('values', [])
-
-    for f in range(7, 42, 7):
-        for i in range(3, 46, 2):
-            block = values[i:f]
-            days.append(block)
-    # Генерим крассивый .json
-    sort = json.dumps(days, sort_keys=True, indent=4)
     return sort
 
 
